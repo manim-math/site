6793c317d68d010d5cb72fd2560bd318ed9f0787
math-manim <dejdevatarovzena@gmail.com>
2025-02-24 21:24:56 +0500

    watchPlayer0

--- videos/watch.js
+++ videos/watch.js
@@ -1,3 +1,16 @@
 var vid = document.getElementById("video");
 var id = URLSearchParams(document.location.search).get("v");
-vid.innerHTML = `<video src=\"${id}\" controls></video>`;
+var lnk;
+fetch(`https://api.github.com/repos/${gitVideoRepo}/contents/`)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(file => {
+        if (file.name.endsWith('.mp4') && file.name.includes(id)){
+            lnk = file.download_url;
+        }
+    });
+});
+vid.innerHTML = `
+<video width=\"90%\" height=\"55%\" controls>
+<source src=\"${lnk}\" type="video/mp4">
+</video>`;
