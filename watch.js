e7e4ec9d33bb21f34d956a55a580ff7eda0ee735
math-manim <dejdevatarovzena@gmail.com>
2025-02-25 00:52:17 +0500

    lOG

--- watch.js
+++ watch.js
@@ -2,10 +2,10 @@ var vid = document.getElementById("video");
 const gitVideoRepo = "manim-math/video-repo";
 var id = new URLSearchParams(document.location.search).get("v");
 console.log(id);
-var lnk = "none";
 fetch(`https://api.github.com/repos/${gitVideoRepo}/contents/`)
   .then(response => response.json())
   .then(data => {
+    var lnk;
     data.forEach(file => {
         if (file.name.endsWith('.mp4') && file.name.replace('.mp4', '') == id){
             console.log("at least im here");
